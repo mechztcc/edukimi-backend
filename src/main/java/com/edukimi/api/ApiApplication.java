2a57dcfc9759229470c8efa9818fcf8025dc7326
mechztcc <dojathan@gmail.com>
2021-03-18 18:23:15 -0300

    Add Cors to all

--- src/main/java/com/edukimi/api/ApiApplication.java
+++ src/main/java/com/edukimi/api/ApiApplication.java
@@ -23,6 +23,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
 
 @SpringBootApplication
 public class ApiApplication implements CommandLineRunner {
